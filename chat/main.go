3c3ca64ce9c0c86c55931de3644768f6d51792e7
Gregory Colella <gregory.colella@gmail.com>
2017-03-15 20:34:53 -0400

    Changed to port 80

--- chat/main.go
+++ chat/main.go
@@ -3,5 +3,5 @@ import "github.com/gdcolella/GobblerExchange/chat/chat_server"
 
 func main() {
 	svr := chat.NewServer(chat.NewBroker(chat.MakeMemoryBuffer(20000)))
-	svr.StartServer(8080)
+	svr.StartServer(80)
 }
