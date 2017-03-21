3292bf86dba6f0dbe8077ce626d4d52a642c7b55
Megan Rigsbee <mrigsbee@vt.edu>
2017-03-21 10:07:13 -0400

    Edit forum post page is now working with database

--- app/controller/forumController.php
+++ app/controller/forumController.php
@@ -20,7 +20,8 @@ class ForumController {
 				break;
 
 			case 'editpost':
-				$this->editpost();
+				$postId = $_GET['postId'];
+				$this->editpost($postId);
 				break;
 			case 'editpost_submit':
 				$this->editpost_submit();
@@ -69,30 +70,28 @@ class ForumController {
 	 * Prereq (POST variables): edit (post id)
 	 * Page variables: title, body, tag
 	 */
-	public function editpost(){
-        SiteController::loggedInCheck();
+	public function editpost($postId){
+    //    SiteController::loggedInCheck();
 
         //retrieve the post
-		$postid = $_POST['edit'];
-		$post_row = ForumPost::loadById($postid);
+		//$postid = $_POST['edit'];
+		$post_row = ForumPost::loadById($postId);
 
         //retrieve post author's username
 		$authorid = $post_row->get('userId');
-		$user = User::loadById($authorid);
-		$username = $user->get('username');
 
 		//check if author of the post is the logged in user
-		if($_SESSION['username'] != $username){
-			$_SESSION['info'] = "You can only edit posts of which you are the author of.";
-			header('Location: '.BASE_URL);
-			exit();
-		} else {
+		// if($_SESSION['userId'] != $authorId){
+		// 	$_SESSION['info'] = "You can only edit posts of which you are the author of.";
+		// 	header('Location: '.BASE_URL);
+		// 	exit();
+		// } else {
 			//allow access to edit post
 			$title = $post_row->get('title');
 			$body = $post_row->get('description');
 			$tag = $post_row->get('tag');
-			include_once SYSTEM_PATH.'/view/edit.tpl';                           //TODO: check tpl is correct
-		}
+			include_once SYSTEM_PATH.'/view/editForumPost.html';                           //TODO: check tpl is correct
+		// }
 	}
 
 	/* Publishes an edited post
