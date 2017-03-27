401cd0361be7163ebe73ae3e33c05c94c6a84bfd
Gregory Colella <gregory.colella@gmail.com>
2017-03-27 16:48:16 -0400

    Implemented chat token negotiation automatically in your session.

--- app/view/templateBottom.php
+++ app/view/templateBottom.php
@@ -4,7 +4,14 @@
 <div class="col-lg-3">
 
 <!-- Chat box -->
-<iframe id="chat" src="http://104.236.205.162/debug_login" style="height: 70vh; width: 20vw;"></iframe>
+      
+<iframe id="chat" src="http://104.236.205.162/" style="height: 70vh; width: 20vw;"></iframe>
+      <script type="text/javascript">
+    var chat_token = "<?php
+echo $_SESSION['chat_token'];
+?>";
+$("#chat").attr('src', "http://104.236.205.162/chat?session="+chat_token);
+      </script>
 </div>
 </div>
 </div>
