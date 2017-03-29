806b32341c58333eaed4b567638e36fa7cd429d3
Gregory Colella <gregory.colella@gmail.com>
2017-03-28 23:43:37 -0400

    Added tropics to the chat UX

--- chat/main.go
+++ chat/main.go
@@ -1,7 +1,12 @@
 package main
-import "github.com/gdcolella/GobblerExchange/chat/chat_server"
+
+import "github.com/Alec-Nelson/GobblerExchange/chat/chat_server"
+import "flag"
+
+var port = flag.Int("port", 80, "Port no to start the server")
 
 func main() {
+	flag.Parse()
 	svr := chat.NewServer(chat.NewBroker(chat.MakeMemoryBuffer(20000)))
-	svr.StartServer(80)
+	svr.StartServer(*port)
 }
