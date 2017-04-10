3b4366e309033bc822e8864847baa31cc1da4fc2
Gregory Colella <gregory.colella@gmail.com>
2017-04-10 01:05:17 -0400

    Continued working on chat system to work on Google infrastructre

--- chat/main.go
+++ chat/main.go
@@ -3,10 +3,11 @@ package main
 import "github.com/Alec-Nelson/GobblerExchange/chat/chat_server"
 import "flag"
 
-var port = flag.Int("port", 8080, "Port no to start the server")
+var port = flag.Int("port", 8081, "Port no to start the server")
 
 func main() {
 	flag.Parse()
 	svr := chat.NewServer(chat.NewBroker(chat.MakeMemoryBuffer(20000)))
+	chat.StartGAEHealthServer()
 	svr.StartServer(*port)
 }
